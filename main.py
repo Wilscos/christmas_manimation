b2fa0e21a13bfd69890589179f7a1497b86b9448
Wilscos <43081060+Wilscos@users.noreply.github.com>
2021-12-23 14:51:59 +0100

    Added image, animated it and added new colors for the text animation

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ from manim import *
 
 class ChristmasMessage(Scene):
     def construct(self):
-        # TODO: add Christmas images and animate them
+        christmas_tree_img = ImageMobject('images/christmas_tree.png')
+        christmas_tree_img.height = 2
+
         text_merry = Text('Merry', color=RED, font_size=84).move_to(2.6 * LEFT)
         text_christmas = Text('Christmas', color=GREEN, font_size=84).next_to(text_merry, RIGHT)
+        christmas_tree_img.next_to(text_christmas, RIGHT)
 
         self.play(FadeIn(text_merry))
         self.play(FadeIn(text_christmas))
+        self.play(FadeIn(christmas_tree_img))
 
-        color_map = ['#165B33', '#146B3A', '#F8B229', '#EA4630', '#BB2528']
-        for _ in range(100):
+        color_map = ['#165B33', '#146B3A', '#F8B229', '#EA4630', '#BB2528',
+                     '#FAFAFA', '#C43A47', '#843145', '#E7D6BA', '#3E6C67',
+                     '#90B594']
+        for _ in range(10):
             # Getting random index for letter in word 'Merry'
             random_merry_letter_idx = random.choice(range(len(text_merry)))
             # Getting random index for letter in word 'Christmas'
@@ -26,6 +32,5 @@ class ChristmasMessage(Scene):
             # Setting random color to random letter in word 'Christmas'
             random_christmas_letter.set_color(random.choice(color_map))
             # Playing this total randomness
-            # TODO: Make the animation faster (hopefully it can go lower than 0.1)
-            self.play(text_merry.animate(run_time=0.1))
-            self.play(text_christmas.animate(run_time=0.1))
+            self.play(text_merry.animate(run_time=0.05))
+            self.play(text_christmas.animate(run_time=0.05))
